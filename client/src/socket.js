b67e9a1a282bd1941eb49732459af6aca2263339
itzzzme <sayandas.devs@gmail.com>
2025-01-25 13:24:45 +0530

    Update socket.js

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,11 +1,11 @@
 import { io } from "socket.io-client";
 
 export const initSocket = async () => {
-    const options = {
-        'force new connection': true,
-        reconnectionAttempts: 'Infinity',
-        timeout: 10000,
-        transports: ['websocket'],
-    };
-    return io("https://realtime-js-code-editor.onrender.com/", options);
-}
+  const options = {
+    "force new connection": true,
+    reconnectionAttempts: "Infinity",
+    timeout: 10000,
+    transports: ["websocket"],
+  };
+  return io("https://realtime-js-code-editor-td8h.onrender.com", options);
+};
